cf2dd3620f15ea232fb84af780a13307974d2621
luckyz <134630746+Biuovo@users.noreply.github.com>
2025-06-04 19:33:33 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -51,8 +51,35 @@ PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
     echo "Adding package: luci-i18n-dockerman-zh-cn"
+# nikki
+if curl -s "$mirror/openwrt/24-config-common" | grep -q "^CONFIG_PACKAGE_luci-app-nikki=y"; then
+    git clone https://$github/morytyann/OpenWrt-nikki package/new/openwrt-nikki --depth=1
+    mkdir -p files/etc/opkg/keys
+    curl -skL https://github.com/nikkinikki-org/OpenWrt-nikki/raw/gh-pages/key-build.pub >files/etc/opkg/keys/ab017c88aab7a08b
+    echo "src/gz nikki https://nikkinikki.pages.dev/openwrt-24.10/$arch/nikki" >>files/etc/opkg/customfeeds.conf
+    mkdir -p files/etc/nikki/run/ui
+    curl -skLo files/etc/nikki/run/Country.mmdb https://$github/NobyDa/geoip/raw/release/Private-GeoIP-CN.mmdb
+    curl -skLo files/etc/nikki/run/GeoIP.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
+    curl -skLo files/etc/nikki/run/GeoSite.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
+    curl -skLo gh-pages.zip https://$github/Zephyruso/zashboard/archive/refs/heads/gh-pages.zip
+    unzip gh-pages.zip
+    mv zashboard-gh-pages files/etc/nikki/run/ui/zashboard
+    rm -rf gh-pages.zip
+    # make sure nikki is always latest
+    git clone -b Alpha --depth=1 https://github.com/metacubex/mihomo --depth=1 nikki
+    nikki_sha=$(git -C nikki rev-parse HEAD)
+    nikki_short_sha=$(git -C nikki rev-parse --short HEAD)
+    git -C nikki config tar.xz.command "xz -c"
+    git -C nikki archive --output=nikki.tar.xz HEAD
+    nikki_checksum=$(sha256sum nikki/nikki.tar.xz | cut -d ' ' -f 1)
+    sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=$(git -C nikki log -n 1 --format=%cs)/" package/new/openwrt-nikki/nikki/Makefile
+    sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=$nikki_sha/" package/new/openwrt-nikki/nikki/Makefile
+    sed -i "s/PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=$nikki_checksum/" package/new/openwrt-nikki/nikki/Makefile
+    sed -i "s/PKG_BUILD_VERSION:=.*/PKG_BUILD_VERSION:=alpha-$nikki_short_sha/" package/new/openwrt-nikki/nikki/Makefile
+    rm -rf nikki
+    
 fi
-
+git clone https://$github/JohnsonRan/packages_net_speedtest-ex package/new/speedtest-ex
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
